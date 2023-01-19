3c93345695a239806fdeec8fe3740cd6ef53596e
JordiCar-design <jordicll@gmail.com>
2023-01-19 15:24:15 +0100

    1er commit

---
+++ codi/pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+      <v-container style="height:100vh" fill-height>
+    <v-row align="center" justify="center" style="height:100%">
+      <v-col align="center">
+        <div style="font-weight:100;font-size:40px;color:brown">
+          <strong>Hello</strong>world!
+        </div>
+      </v-col>
+    </v-row>
+  </v-container> 
+</template>
